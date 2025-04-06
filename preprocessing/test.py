d2647163659d0c89fb33b7f01d1cee04539f6361
oukunhei <okhou533@connect.hkust-gz.edu.cn>
2025-04-06 16:44:03 +0800

    Update test.py

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -1,6 +1,1 @@
-if __name__ == "__main__":
-    vectorizer = TextVectorizer(n_features=2**20)
-    train_features, valid_features = vectorizer.preprocess("0000.parquet", "validate_0000.parquet")
-    
-    print("训练特征形状:", train_features.shape)
-    print("验证特征形状:", valid_features.shape)
+
