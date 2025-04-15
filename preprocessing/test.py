881d849a73069514396e0a7c51fb2335cdeeba82
oukunhei <okhou533@connect.hkust-gz.edu.cn>
2025-04-15 18:41:37 +0800

    Update test.py

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -1,9 +1,12 @@
-from vectorizer import TextVectorizer
+from preprocessor import TextPreprocessor
 
 if __name__ == "__main__":
-    preprocessor = TextVectorizer(n_features=2**20)  # 可以调整特征数量
-    train_features, valid_features = preprocessor.preprocess("0000.parquet", "validate_0000.parquet")
-
-    # train_features 和 valid_features 是稀疏矩阵，可以用于后续分析或建模
-    print("训练特征形状:", train_features.shape)
-    print("验证特征形状:", valid_features.shape)
+    preprocessor = TextPreprocessor(n_features=2**20, ngram_range=(3, 5))
+    preprocessor.load_data('train.parquet', 'valid.parquet')
+    preprocessor.apply_cleaning()
+    train_features, valid_features = preprocessor.feature_extraction()
+    #input for minhash
+    train_ngrams, valid_ngrams = preprocessor.get_ngrams_for_minhash()
+    #input for simhash
+    train_token_freqs, valid_token_freqs = preprocessor.get_simhash_inputs()
+    train_features, valid_features = preprocessor.preprocess('train.parquet', 'valid.parquet')
